bc98eb6d32c3d39fae5d5addc74a2017dd261ea0
Youngjun Choi <juni2004c@gmail.com>
2024-07-19 23:15:30 +0900

    fix: change test case to use constant date in flattenObject test cases

--- src/object/flattenObject.spec.ts
+++ src/object/flattenObject.spec.ts
@@ -1,18 +1,19 @@
-import { describe, expect, it, test } from "vitest"
-import { flattenObject } from "./flattenObject";
+import { describe, expect, it } from 'vitest';
+import { flattenObject } from './flattenObject';
 
 describe('flattenObject', function () {
   it('flattens primitive values correctly', () => {
     const result1 = flattenObject({
       a: {
-        b: 'yay'
-      }
+        b: 'yay',
+      },
     });
 
     expect(result1).toEqual({
-      'a.b': 'yay'
-    })
+      'a.b': 'yay',
+    });
 
+    const date = new Date();
     const result2 = flattenObject({
       a: {
         b: {
@@ -21,10 +22,10 @@ describe('flattenObject', function () {
           boolean: true,
           null: null,
           undefined: undefined,
-          date: new Date(),
-        }
-      }
-    })
+          date: date,
+        },
+      },
+    });
 
     expect(result2).toEqual({
       'a.b.string': 'hello world',
@@ -32,11 +33,12 @@ describe('flattenObject', function () {
       'a.b.boolean': true,
       'a.b.null': null,
       'a.b.undefined': undefined,
-      'a.b.date': new Date(),
-    })
-  })
+      'a.b.date': date,
+    });
+  });
 
   it('flattens multiple keys', () => {
+    const date = new Date();
     const result = flattenObject({
       a: {
         b: {
@@ -45,58 +47,58 @@ describe('flattenObject', function () {
         d: {
           e: {
             f: {
-              g: new Date()
-            }
-          }
-        }
+              g: date,
+            },
+          },
+        },
       },
       h: {
-        i: 'hi'
-      }
+        i: 'hi',
+      },
     });
 
     expect(result).toEqual({
       'a.b.c': 1,
-      'a.d.e.f.g': new Date(),
-      'h.i': 'hi'
-    })
-  })
+      'a.d.e.f.g': date,
+      'h.i': 'hi',
+    });
+  });
 
   it('handles empty objects correctly', () => {
     const result = flattenObject({
       a: {
-        b: {}
-      }
+        b: {},
+      },
     });
 
     expect(result).toEqual({
       'a.b': {},
-    })
-  })
+    });
+  });
 
   it('handles `Buffer`s correctly', () => {
     const result = flattenObject({
       a: {
-        b: Buffer.from('test')
-      }
+        b: Buffer.from('test'),
+      },
     });
 
     expect(result).toEqual({
-      'a.b': Buffer.from('test')
+      'a.b': Buffer.from('test'),
     });
-  })
+  });
 
   it('handles `TypedArray`s correctly', () => {
     const result = flattenObject({
       a: {
-        b: new Uint8Array([1, 2, 3, 4])
-      }
+        b: new Uint8Array([1, 2, 3, 4]),
+      },
     });
 
     expect(result).toEqual({
-      'a.b': new Uint8Array([1, 2, 3, 4])
+      'a.b': new Uint8Array([1, 2, 3, 4]),
     });
-  })
+  });
 
   it('handles numeric keys', () => {
     const result = flattenObject({
@@ -104,31 +106,31 @@ describe('flattenObject', function () {
         '02': {
           '03': 1,
         },
-      }
+      },
     });
 
     expect(result).toEqual({
       '01.02.03': 1,
     });
-  })
+  });
 
   it('handles mixed keys', () => {
     const result = flattenObject({
-      'a1': {
-        'b2': {
-          'c3': 1,
+      a1: {
+        b2: {
+          c3: 1,
         },
-      }
+      },
     });
 
     expect(result).toEqual({
-      'a1.b2.c3': 1
+      'a1.b2.c3': 1,
     });
-  })
+  });
 
   it('handles arrays', () => {
     const result = flattenObject({
-      a: [1, 2, 3]
+      a: [1, 2, 3],
     });
 
     expect(result).toEqual({
@@ -136,5 +138,5 @@ describe('flattenObject', function () {
       'a.1': 2,
       'a.2': 3,
     });
-  })
-})
+  });
+});
